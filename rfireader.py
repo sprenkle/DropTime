c90e95e1b5fb0b39a5b13c1d2ebf2cb28816445c
david sprenkle <david@sprenkle.net>
2019-03-20 18:31:36 -0400

    added fixes

--- rfireader.py
+++ rfireader.py
@@ -1,4 +1,11 @@
+import RPi.GPIO as GPIO
+import SimpleMFRC522
+
 class RfiReader:
     # arr = array.array('i', [1, 2, 3])
+    def __init__(self):
+        self.reader = SimpleMFRC522.SimpleMFRC522()
+
     def read_card(self):
-        return '369007'
+        id, text = self.reader_card()
+        return str(id)
--- timeularaction.py
+++ timeularaction.py
@@ -7,13 +7,17 @@ class TimeularAction:
         self.api = TimularApi("NDcwMDBfYzU5MTUwMDQ2OWU4NDA4OWExZjFlMTZlNDhlNjFlMDM=",
                          "NDJkNDY1MjZhMDk5NDAyZTg2YjNkNWIyNDVmYmFiYjc=")
         self.task_dict = {"231965344320": "369007", "438308258332": "369008", "25991398012": "369006"}
+        self.running_tag_id = None
 
     def is_actionable(self, tag_id):
         return tag_id in self.task_dict
 
     def execute(self, tag_id):
-        print(tag_id)
+        if tag_id is None:
+            if self.running_tag_id is not None:
+                self.api.stop_tracking(self.task_dict[str(tag_id)])
         if tag_id not in self.task_dict:
             print(tag_id + " Not Valid")
             return
+        self.running_tag_id = tag_id
         self.api.start_tracking(self.task_dict[tag_id])
