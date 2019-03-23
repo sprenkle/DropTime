9efdfb5a0360c508e3f58e288f132a8ac5bea602
david sprenkle <david@sprenkle.net>
2019-03-23 00:08:38 -0400

    update

--- cardrest.py
+++ cardrest.py
@@ -44,10 +44,41 @@ class Devices(Resource):
         conn.execute(querystring)  # This line performs query and returns json result
         return jsonify({"results": "ok"})
 
+class Tags(Resource):
+    def get(self):
+        conn = db_connect.connect()  # connect to database
+        query = conn.execute("select * from tags")  # This line performs query and returns json result
+        return {'tags': [i for i in query.cursor.fetchall()]}  # Fetches first column that is Employee ID
+
+    def post(self):
+        tag = request.json
+        print(tag)
+        conn = db_connect.connect()  # connect to database
+        querystring = "INSERT INTO tags (userid, name, description) VALUES ('{}','{}','{}')"\
+            .format(tag["userid"], tag["name"], tag["description"])
+        conn.execute(querystring)  # This line performs query and returns json result
+        return jsonify({"results": "ok"})
+
+class Activities(Resource):
+    def get(self):
+        conn = db_connect.connect()  # connect to database
+        query = conn.execute("select * from activities")  # This line performs query and returns json result
+        return {'activities': [i for i in query.cursor.fetchall()]}  # Fetches first column that is Employee ID
+
+    def post(self):
+        activity = request.json
+        print(activity)
+        conn = db_connect.connect()  # connect to database
+        querystring = "INSERT INTO activities (userid, name, color, show, integration, dailygoals, dailytimeSec) VALUES ('{}','{}','{}','{}','{}','{}','{}')"\
+            .format(activity["userid"], activity["name"], activity["color"], activity["show"], activity["integration"], activity["dailygoals"], activity["dailytimeSec"])
+        conn.execute(querystring)  # This line performs query and returns json result
+        return jsonify({"results": "ok"})
 
 
 api.add_resource(Users, '/users')  # Route_1
 api.add_resource(Devices, '/devices')  # Route_1
+api.add_resource(Tags, '/tags')  # Route_1
+api.add_resource(Activities, '/activities')  # Route_1
 
 
 if __name__ == '__main__':
