6331313fa4c34e3e7b609dddbc447dbf4d18047a
david sprenkle <david@sprenkle.net>
2019-03-28 11:39:58 -0400

    ewrter

--- leddevice.py
+++ leddevice.py
@@ -8,7 +8,7 @@ class LedDevice:
         pixel_pin = board.D18
         self.num_pixels = 24
         order = neopixel.GRB
-        self.pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.2, auto_write=False,
+        self.pixels = neopixel.NeoPixel(pixel_pin, self.num_pixels, brightness=0.2, auto_write=False,
                                    pixel_order=order)
 
     def show(self, led_patterns):
