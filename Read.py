fd470ba6e5818a539e06de809a44b3b8826b291c
david sprenkle <david@sprenkle.net>
2019-04-06 16:58:07 -0400

    update

--- Read.py
+++ Read.py
@@ -1,81 +1,86 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-#
-#    Copyright 2014,2018 Mario Gomez <mario.gomez@teubi.co>
-#
-#    This file is part of MFRC522-Python
-#    MFRC522-Python is a simple Python implementation for
-#    the MFRC522 NFC Card Reader for the Raspberry Pi.
-#
-#    MFRC522-Python is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Lesser General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    MFRC522-Python is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Lesser General Public License for more details.
-#
-#    You should have received a copy of the GNU Lesser General Public License
-#    along with MFRC522-Python.  If not, see <http://www.gnu.org/licenses/>.
-#
-
-import RPi.GPIO as GPIO
 import MFRC522
-import signal
+import RPi.GPIO as GPIO
+import time
 
-continue_reading = True
 
-# Capture SIGINT for cleanup when the script is aborted
-def end_read(signal,frame):
-    global continue_reading
-    print "Ctrl+C captured, ending read."
-    continue_reading = False
-    GPIO.cleanup()
+class Read:
+    READER = None;
 
-# Hook the SIGINT
-signal.signal(signal.SIGINT, end_read)
+    KEY = [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF]
+    BLOCK_ADDRS = [8, 9, 10]
 
-# Create an object of the class MFRC522
-MIFAREReader = MFRC522.MFRC522()
+    def __init__(self):
+        self.READER = MFRC522.MFRC522()
 
-# Welcome message
-print "Welcome to the MFRC522 data read example"
-print "Press Ctrl-C to stop."
+    def read(self):
+        id, text = self.read_no_block()
+        while not id:
+            id, text = self.read_no_block()
+        return id, text
 
-# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-while continue_reading:
-    
-    # Scan for cards    
-    (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
+    def read_id(self):
+        id, text = self.read_no_block()
+        while not id:
+            id, text = self.read_no_block()
+        return id
 
-    # If a card is found
-    if status == MIFAREReader.MI_OK:
-        print "Card detected"
-    
-    # Get the UID of the card
-    (status,uid) = MIFAREReader.MFRC522_Anticoll()
+    def read_id_no_block(self):
+        id, text = self.read_no_block()
+        return id
 
-    # If we have the UID, continue
-    if status == MIFAREReader.MI_OK:
+    def read_no_block(self):
+        (status, TagType) = self.READER.MFRC522_Request(self.READER.PICC_REQIDL)
+        if status != self.READER.MI_OK:
+            return None, None
+        (status, uid) = self.READER.MFRC522_Anticoll()
+        if status != self.READER.MI_OK:
+            return None, None
+        id = self.uid_to_num(uid)
+        self.READER.MFRC522_SelectTag(uid)
+        status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+        data = []
+        text_read = ''
+        if status == self.READER.MI_OK:
+            for block_num in self.BLOCK_ADDRS:
+                block = self.READER.MFRC522_Read(block_num)
+                if block:
+                    data += block
+            if data:
+                text_read = ''.join(chr(i) for i in data)
+        self.READER.MFRC522_StopCrypto1()
+        time.sleep(0.1)
+        return id, text_read
 
-        # Print UID
-        print "Card read UID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3])
-    
-        # This is the default key for authentication
-        key = [0xFF,0xFF,0xFF,0xFF,0xFF,0xFF]
-        
-        # Select the scanned tag
-        MIFAREReader.MFRC522_SelectTag(uid)
+    def write(self, text):
+        id, text_in = self.write_no_block(text)
+        while not id:
+            id, text_in = self.write_no_block(text)
+        return id, text_in
 
-        # Authenticate
-        status = MIFAREReader.MFRC522_Auth(MIFAREReader.PICC_AUTHENT1A, 8, key, uid)
+    def write_no_block(self, text):
+        (status, TagType) = self.READER.MFRC522_Request(self.READER.PICC_REQIDL)
+        if status != self.READER.MI_OK:
+            return None, None
+        (status, uid) = self.READER.MFRC522_Anticoll()
+        if status != self.READER.MI_OK:
+            return None, None
+        id = self.uid_to_num(uid)
+        self.READER.MFRC522_SelectTag(uid)
+        status = self.READER.MFRC522_Auth(self.READER.PICC_AUTHENT1A, 11, self.KEY, uid)
+        self.READER.MFRC522_Read(11)
+        if status == self.READER.MI_OK:
+            data = bytearray()
+            data.extend(bytearray(text.ljust(len(self.BLOCK_ADDRS) * 16).encode('ascii')))
+            i = 0
+            for block_num in self.BLOCK_ADDRS:
+                self.READER.MFRC522_Write(block_num, data[(i * 16):(i + 1) * 16])
+                i += 1
+        self.READER.MFRC522_StopCrypto1()
+        time.sleep(0.1)
+        return id, text[0:(len(self.BLOCK_ADDRS) * 16)]
 
-        # Check if authenticated
-        if status == MIFAREReader.MI_OK:
-            MIFAREReader.MFRC522_Read(8)
-            MIFAREReader.MFRC522_StopCrypto1()
-        else:
-            print "Authentication error"
-
+    def uid_to_num(self, uid):
+        n = 0
+        for i in range(0, 5):
+            n = n * 256 + uid[i]
+        return n
--- rfireader.py
+++ rfireader.py
@@ -1,13 +1,11 @@
 import RPi.GPIO as GPIO
-import SimpleMFRC522
-import MFRC522
+from Read import Reader
 
 
 class RfiReader:
     # arr = array.array('i', [1, 2, 3])
     def __init__(self):
-        self.reader = SimpleMFRC522.SimpleMFRC522()
-        self.mfrc522 = MFRC522.MFRC522()
+        self.reader = Reader()
 
     def read_card(self):
         tag_id = self.reader.read_id_no_block()
@@ -16,19 +14,13 @@ class RfiReader:
             quit()
         return tag_id
 
+
     def read_card_block(self):
-        uid = None
-        while not uid:
-            (status, TagType) = self.mfrc522.MFRC522_Request(self.mfrc522.PICC_REQIDL)
-            if status != self.mfrc522.MI_OK:
-                return None
-            (status, uid) = self.mfrc522.MFRC522_Anticoll()
-            if status != self.mfrc522.MI_OK:
-                return None
-        return uid
+        tag_id = self.reader.read_id_no_block()
+        print(tag_id)
+        GPIO.cleanup()
 
 
 if __name__ == "__main__":
     reader = RfiReader()
-    print(str(reader.read_card_block()))
-
+    reader.read_card_block()
