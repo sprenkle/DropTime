cbdfc86a477de03dffc36465abb7c3049b8b9b49
david sprenkle <david@sprenkle.net>
2019-04-06 20:41:43 -0400

    update

--- rfireader.py
+++ rfireader.py
@@ -1,15 +1,24 @@
 import RPi.GPIO as GPIO
-import SimpleMFRC522
+import MFRC522
+import signal
 
 
 class RfiReader:
     # arr = array.array('i', [1, 2, 3])
     def __init__(self):
-        self.reader = SimpleMFRC522.SimpleMFRC522()
+        self.MIFAREReader = MFRC522.MFRC522()
 
     def read_card(self):
-        tag_id = self.reader.read_id_no_block()
-        if tag_id is not None and tag_id == 474735937816:
-            GPIO.cleanup()
-            quit()
-        return tag_id
+        (status, TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
+        (status, uid) = self.MIFAREReader.MFRC522_Anticoll()
+        if status == self.MIFAREReader.MI_OK:
+            return "%s-%s-%s-%s" % (uid[0], uid[1], uid[2], uid[3])
+        return None
+
+
+if __name__ == "__main__":
+    reader = RfiReader()
+    value = reader.read_card()
+    while value is None:
+        value = reader.read_card()
+    print(value)
