78762bdae4ab5098e4292cecc1a5a75801520fd3
david sprenkle <david@sprenkle.net>
2019-04-06 20:54:59 -0400

    update

--- rfireader.py
+++ rfireader.py
@@ -12,7 +12,7 @@ class RfiReader:
         (status, TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
         (status, uid) = self.MIFAREReader.MFRC522_Anticoll()
         if status == self.MIFAREReader.MI_OK:
-            return "%s-%s-%s-%s" % (uid[0], uid[1], uid[2], uid[3])
+            return uid[0] + (uid[1] << 8) + (uid[2] << 16) + (uid[3] << 24 )
         return None
 
 
