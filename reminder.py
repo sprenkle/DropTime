f80849fd9ae88e4aa20de26aef17fa7b987cb8a6
david sprenkle <david@sprenkle.net>
2019-04-24 22:04:40 -0400

    update

--- reminder.py
+++ reminder.py
@@ -14,7 +14,7 @@ class Reminder:
         self.led_controller = led_controller
         self.has_reminder = False
         self.reminders = []
-        self.next_update = datetime.now() + timedelta(minutes=5)
+        self.next_update = datetime.now() 
         self.reminders = self.tag_repository.get_reminders(self.device_id)
 
     # updates the reminders from repository
