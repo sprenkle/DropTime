0287102c8cf3c8c0f98ac4ecbf31ddeb90dda0a7
david sprenkle <david@sprenkle.net>
2019-04-27 08:05:22 -0400

    update

--- rfidevice.py
+++ rfidevice.py
@@ -11,7 +11,7 @@ class RfiDevice:
         self.none_count = 0
         self.last_card_id = None
         self.retries = int(configuration.get_value("rfireader", "retries"))
-        logging.INFO("RFI Device retries = {}".format(str(self.retries)))
+        logging.info("RFI Device retries = {}".format(str(self.retries)))
 
     def read_tag(self):
         (status, TagType) = self.MIFAREReader.MFRC522_Request(self.MIFAREReader.PICC_REQIDL)
@@ -19,7 +19,7 @@ class RfiDevice:
         card_id = None
         if status == self.MIFAREReader.MI_OK:
             card_id = uid[0] + (uid[1] << 8) + (uid[2] << 16) + (uid[3] << 24)
-        logging.DEBUG("RFI Device read {} none_count={}".format(card_id, self.none_count))
+        logging.debug("RFI Device read {} none_count={}".format(card_id, self.none_count))
 
         if card_id is None:
             self.none_count = self.none_count + 1
