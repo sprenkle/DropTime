db56c2cd78dde7b9e1e702ce346371a8d1936058
david sprenkle <david@sprenkle.net>
2019-05-05 11:53:14 -0400

    update

--- rfidevice.py
+++ rfidevice.py
@@ -9,6 +9,8 @@ class RfiDevice:
     # arr = array.array('i', [1, 2, 3])
     def __init__(self, configuration):
         self.MIFAREReader = MFRC522.MFRC522()
+        self.none_count = 0
+        self.retries = int(configuration.get_value("rfireader", "retries"))
         self.last_card_id = None
         self.last_card_delay = int(configuration.get_value("rfireader", "delay"))
         self.last_time_read = datetime.datetime.now()
@@ -22,12 +24,15 @@ class RfiDevice:
             card_id = uid[0] + (uid[1] << 8) + (uid[2] << 16) + (uid[3] << 24)
 
         if card_id is not None:
+            self.none_count = 0
             self.last_time_read = datetime.datetime.now()
+        else:
+            self.none_count = self.none_count + 1
 
         logging.debug("RFI Device read {} {}".format(card_id, (datetime.datetime.now() - self.last_time_read).seconds))
 
-        if card_id is not None or self.last_time_read < datetime.datetime.now() - \
-                datetime.timedelta(seconds=self.last_card_delay):
+        if card_id is not None or (self.last_time_read < datetime.datetime.now() - \
+                datetime.timedelta(seconds=self.last_card_delay) and self.none_count > self.retries):
             self.last_card_id = card_id
             return card_id
 
