e7d4548acc94834316a67bfd81ef163842c46667
david sprenkle <david@sprenkle.net>
2020-04-26 08:59:07 -0400

    asdf

--- reminder.py
+++ reminder.py
@@ -68,6 +68,7 @@ class Reminder:
         self.tags_seen[tag_to_action["identifier"]] = datetime.now();
 
         logging.info("tags_to_action {} {}".format(tag_to_action["identifier"], self.tags_seen[tag_to_action["identifier"]]))
+        logging.info("self.tags_seen {}".format(self.tags_seen))
 
     def has_reminders(self):
         return len(self.reminders) > 0
